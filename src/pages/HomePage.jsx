a165da63e873dc739d95a1afa8015072da20e52a
HildanRizkiBV2 <hildanrizki16@gmail.com>
2024-01-23 06:51:36 +0700

    add typewriter

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import { Row ,Container , Col} from 'react-bootstrap';
 import HeroImage from '../assets/img/hero.png'
+import Typewriter from 'typewriter-effect';
+import cursor from 'typewriter-effect';
 
 const HomePage = () => {
 
@@ -9,7 +11,7 @@ const HomePage = () => {
         <Container>
          <Row className="header-box d-flex align-items-center">
           <Col lg="6">
-            <h1>I am
+            <h1>I am <Typewriter options={{ strings:['Hildan', 'Developer','Front-end'],autoStart: true,loop: true,}}/>
             </h1>
             <h1 className="mb-2">belajar <span>cooding </span> di sini</h1>
             <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quae sed perferendis recusandae obcaecati quos.</p>
